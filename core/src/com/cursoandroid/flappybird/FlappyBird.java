5bb0d5a3879c40e686f14d1049e71f42783b30fc
Vitória Camila <vcamilaxs@gmail.com>
2020-04-26 17:40:41 -0300

    Criação do Projeto

---
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -0,0 +1,58 @@
+package com.cursoandroid.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private Texture[] passaros;
+	private Texture fundo;
+
+	//Atributos de configuração
+	private int movimento = 0;
+	private int larguraDispositivo;
+	private int alturaDispositivo;
+
+	private float variacao = 0;
+	private float velocidadeQueda = 0;
+	private float posicaoInicialVertical;
+	private float posicaoInicialHorizontal;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+		passaros[1] = new Texture("passaro2.png");
+		passaros[2] = new Texture("passaro3.png");
+
+		fundo = new Texture("fundo.png");
+
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+		posicaoInicialVertical = alturaDispositivo/2;
+		posicaoInicialHorizontal = 30;
+	}
+
+	@Override
+	public void render () {
+		variacao += Gdx.graphics.getDeltaTime()*10;
+
+		if(variacao > 2) variacao = 0; //if antenário
+
+		batch.begin();
+
+		batch.draw(fundo, 0,0, larguraDispositivo, alturaDispositivo);
+		batch.draw(passaros[(int) variacao], 30,posicaoInicialVertical);
+
+
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
