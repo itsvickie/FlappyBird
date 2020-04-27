1b0a1407d0425faceb388e92107b3bf1ef3ab081
Vitória Camila <vcamilaxs@gmail.com>
2020-04-27 16:53:20 -0300

    Atualização - Canos e Colisões
    Criação dos Canos e das Formas para utilizar na Colisão do Pássaro

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -2,20 +2,29 @@ package com.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch;
+	private ShapeRenderer shape;
 
 	//Declaração das Texturas
 	private Texture[] passaros;
 	private Texture fundo;
 	private Texture canoBaixo;
 	private Texture canoTopo;
+	private BitmapFont fonte;
+	private Circle passaroCirculo;
+	private Rectangle retanguloCanoBaixo;
+	private Rectangle retanguloCanoTopo;
 
 	//Atributos de Configuração
 	private int larguraDispositivo;
@@ -23,6 +32,8 @@ public class FlappyBird extends ApplicationAdapter {
 	private float deltaTime;
 	private Random numeroRandomico;
 	private int alturaEntreCanosRandom;
+	private int score = 0;
+	private boolean marcouPonto;
 
 	//Configurações de Movimento
 	private float variacao = 0;
@@ -30,6 +41,7 @@ public class FlappyBird extends ApplicationAdapter {
 	private float posicaoInicialVertical;
 	private float posicaoMovimentoCanoHorizontal;
 	private float espacoCanos;
+	private int estadoJogo = 0;
 
 	@Override
 	public void create () {
@@ -40,36 +52,71 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		deltaTime = Gdx.graphics.getDeltaTime();
-		configCanoTexturas();
-		configPassaroTexturas();
-
-		if (Gdx.input.justTouched()) velocidadeQueda = -15;
+		if (estadoJogo == 0){
+			if(Gdx.input.justTouched()) estadoJogo = 1;
+		} else {
+			deltaTime = Gdx.graphics.getDeltaTime();
+			configCanoTexturas();
+			configPassaroTexturas();
+			if (Gdx.input.justTouched()) velocidadeQueda = -15;
+			verificarPontuacao();
+		}
 
 		//INICIANDO A RENDERIZAÇÃO
 		batch.begin();
 
 		//Chamando as texturas com parâmetros
 		batch.draw(fundo, 0,0, larguraDispositivo, alturaDispositivo);
-		batch.draw(passaros[(int) variacao], 30, posicaoInicialVertical);
+		batch.draw(passaros[(int) variacao], 100, posicaoInicialVertical);
 		batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo/2 + espacoCanos/2 + alturaEntreCanosRandom);
 		batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 - canoBaixo.getHeight() - espacoCanos/2 + alturaEntreCanosRandom);
+		fonte.draw(batch, String.valueOf(score), larguraDispositivo/2, alturaDispositivo - 100);
 
 		//FINALIZANDO A RENDERIZAÇÃO
 		batch.end();
+
+		desenharFormas();
 	}
 
 	private void inicializarTexturas(){
         //DECLARAÇÃO DE TEXTURAS
+		//Inicialização dos Pássaros
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro3.png");
 
+		//Inicialização do Fundo
 		fundo = new Texture("fundo.png");
 
+		//Inicialização dos Canos
 		canoBaixo = new Texture("cano_baixo.png");
 		canoTopo = new Texture("cano_topo.png");
+
+		//Criação da Fonte e suas características
+		fonte = new BitmapFont();
+		fonte.setColor(Color.WHITE);
+		fonte.getData().setScale(6);
+
+		//Círculo - Colisão
+		passaroCirculo = new Circle();
+
+		//Retangulo Baixo - Colisão
+
+
+		//Retangulo Topo - Colisão
+		retanguloCanoTopo = new Rectangle(
+				posicaoMovimentoCanoHorizontal, //DECLARAÇÃO DO X
+				alturaDispositivo/2 + espacoCanos/2 + alturaEntreCanosRandom, //DECLARAÇÃO DO Y
+				canoTopo.getWidth(), //DECLARAÇÃO DA LARGURA
+				canoTopo.getHeight() //DECLARAÇÃO DA ALTURA
+		);
+
+		//Retangulo Topo - Colisão
+		retanguloCanoTopo = new Rectangle();
+
+		//Inicialização do ShapeRender
+		shape = new ShapeRenderer();
 	}
 
 	private void inicializarConfiguracoes(){
@@ -101,11 +148,61 @@ public class FlappyBird extends ApplicationAdapter {
     }
 
     private void configCanoTexturas(){
-		posicaoMovimentoCanoHorizontal -= deltaTime*200;
+		posicaoMovimentoCanoHorizontal -= deltaTime*500;
 
 		if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
 			posicaoMovimentoCanoHorizontal = larguraDispositivo;
 			alturaEntreCanosRandom = numeroRandomico.nextInt(400) - 200;
+			marcouPonto = false;
 		}
     }
+
+    private void verificarPontuacao(){
+		if(posicaoMovimentoCanoHorizontal < 100){
+			if(!marcouPonto) score++; marcouPonto = true;
+		}
+	}
+
+	private void desenharFormas(){
+        //Inicialização do ShapeRender
+        shape = new ShapeRenderer();
+
+        //Círculo - Forma
+        passaroCirculo = new Circle();
+        passaroCirculo.set(100 + passaros[0].getWidth()/2, posicaoInicialVertical + passaros[0].getHeight()/2, passaros[0].getWidth()/2);
+
+        //Retangulo Baixo - Forma
+        retanguloCanoBaixo = new Rectangle(
+                posicaoMovimentoCanoHorizontal, //DECLARAÇÃO DO X
+                alturaDispositivo /2 - canoBaixo.getHeight() - espacoCanos/2 + alturaEntreCanosRandom, //DECLARAÇÃO DO Y
+                canoBaixo.getWidth(), //DECLARAÇÃO DA LARGURA
+                canoBaixo.getHeight() //DECLARAÇÃO DA ALTURA
+        );
+
+        //Retangulo Topo - Forma
+        retanguloCanoTopo = new Rectangle(
+                posicaoMovimentoCanoHorizontal, //DECLARAÇÃO DO X
+                alturaDispositivo/2 + espacoCanos/2 + alturaEntreCanosRandom, //DECLARAÇÃO DO Y
+                canoTopo.getWidth(), //DECLARAÇÃO DA LARGURA
+                canoTopo.getHeight() //DECLARAÇÃO DA ALTURA
+        );
+
+        //Cor da Forma
+		shape.setColor(Color.RED);
+
+		//Inicialiação das Formas
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+
+		//Círculo
+		shape.circle(passaroCirculo.x, passaroCirculo.y, passaroCirculo.radius);
+
+		//Retângulo Baixo
+		shape.rect(retanguloCanoBaixo.x, retanguloCanoBaixo.y, retanguloCanoBaixo.width, retanguloCanoBaixo.height);
+
+		//Retângulo Topo
+		shape.rect(retanguloCanoTopo.x, retanguloCanoTopo.y, retanguloCanoTopo.width, retanguloCanoTopo.height);
+
+		//Finalização das Forma
+		shape.end();
+	}
 }
